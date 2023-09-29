d78aee7222061c623dbf8710939449b2c5606d42
dhson <saozzang@gmail.com>
2023-09-30 00:22:38 +0900

    [#3-1]FrogJmp

---
+++ src/main/java/timeComplexity/Main.java
@@ -0,0 +1,11 @@
+package timeComplexity;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FrogJmp frogJmp = new FrogJmp();
+        int result = frogJmp.solution(10, 85, 30);
+        System.out.println("=====> result: " + result);
+    }
+}
