1eb405454207bad9c48d73b44c73faec5d260f8b
dhson <saozzang@gmail.com>
2023-09-30 18:32:42 +0900

    [#3-3]TapeEquilibrium

--- src/main/java/timeComplexity/Main.java
+++ src/main/java/timeComplexity/Main.java
@@ -8,9 +8,14 @@ public class Main {
 //        int result = frogJmp.solution(10, 85, 30);
 //        System.out.println("=====> result: " + result);
 
-        PermMissingElem permMissingElem = new PermMissingElem();
-        int[] A = {2, 1};
-        int result = permMissingElem.solution(A);
+//        PermMissingElem permMissingElem = new PermMissingElem();
+//        int[] A = {2, 1};
+//        int result = permMissingElem.solution(A);
+//        System.out.println("=====> result: " + result);
+
+        TapeEquilibrium tapeEquilibrium = new TapeEquilibrium();
+        int[] A= {3, 1, 2, 4, 3};
+        int result = tapeEquilibrium.solution(A);
         System.out.println("=====> result: " + result);
     }
 }
