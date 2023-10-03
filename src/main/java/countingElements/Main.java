fc4aec0084828edac63695231322be81a44c3a67
dhson <saozzang@gmail.com>
2023-10-04 00:43:09 +0900

    [#4-3]MaxCounters

--- src/main/java/countingElements/Main.java
+++ src/main/java/countingElements/Main.java
@@ -1,5 +1,7 @@
 package countingElements;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,9 +11,14 @@ public class Main {
 //        int result = frogRiveOne.solution(5, A);
 //        System.out.println("=====> result: " + result);
 
-        PermCheck permCheck = new PermCheck();
-        int[] A = {4, 1, 3};
-        int result = permCheck.solution(A);
-        System.out.println("=====> result: " + result);
+//        PermCheck permCheck = new PermCheck();
+//        int[] A = {4, 1, 3};
+//        int result = permCheck.solution(A);
+//        System.out.println("=====> result: " + result);
+
+        MaxCounters maxCounters = new MaxCounters();
+        int[] A = {3, 4, 4, 6, 1, 4, 4};
+        int[] result = maxCounters.solution(5, A);
+        System.out.println("=====> result: " + Arrays.toString(result));
     }
 }
