ab2923542208c4060ac27680f2c13417111c3274
dhson <saozzang@gmail.com>
2023-10-04 23:11:11 +0900

    [#5-2]CountDiv

--- src/main/java/prefixSums/Main.java
+++ src/main/java/prefixSums/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void  main(String[] args) {
 
-        PassingCars passingCars = new PassingCars();
-        int[] A = {0, 1, 0, 1, 1 };
-        System.out.println("=====> result: " + passingCars.solution(A));
+//        PassingCars passingCars = new PassingCars();
+//        int[] A = {0, 1, 0, 1, 1 };
+//        System.out.println("=====> result: " + passingCars.solution(A));
+
+        CountDiv countDiv = new CountDiv();
+        System.out.println("=====> result: " + countDiv.solution(6, 11, 2));
     }
 }
