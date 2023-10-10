ce344745c5547600afc73c49462f9be75954ec5d
dhson <saozzang@gmail.com>
2023-10-10 22:38:52 +0900

    [#5-4]MinAvgTwoSlice

--- src/main/java/prefixSums/Main.java
+++ src/main/java/prefixSums/Main.java
@@ -13,12 +13,16 @@ public class Main {
 //        CountDiv countDiv = new CountDiv();
 //        System.out.println("=====> result: " + countDiv.solution(6, 11, 2));
 
-        GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
-        String S = "CAGCCTA";
-        int[] P = {2, 5, 0};
-        int[] Q = {4, 5, 6};
-        int[] result = genomicRangeQuery.solution(S, P, Q);
+//        GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
+//        String S = "CAGCCTA";
+//        int[] P = {2, 5, 0};
+//        int[] Q = {4, 5, 6};
+//        int[] result = genomicRangeQuery.solution(S, P, Q);
+//        System.out.println("=====> result: " + Arrays.toString(result));
 
-        System.out.println("=====> result: " + Arrays.toString(result));
+        MinAvgTwoSlice minAvgTwoSlice = new MinAvgTwoSlice();
+        int[] A = {4, 2, 2, 5, 1, 5, 8};
+        int result = minAvgTwoSlice.solution(A);
+        System.out.println("=====> result: " + result);
     }
 }
