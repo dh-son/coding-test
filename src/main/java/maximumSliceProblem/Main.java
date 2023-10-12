5032d501583ba19fa778adf38dbc86a09dacae0d
dhson <saozzang@gmail.com>
2023-10-12 23:27:54 +0900

    [#9-1]MaxProfit

---
+++ src/main/java/maximumSliceProblem/Main.java
@@ -0,0 +1,11 @@
+package maximumSliceProblem;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MaxProfit maxProfit = new MaxProfit();
+        int[] A1 = {23171, 21011, 21123, 21366, 21013, 21367};
+        System.out.println("=====> result: " + maxProfit.solution(A1));
+    }
+}
