33ac0ae1575e49610a1f0ef38eaa3e99af0fb437
dhson <saozzang@gmail.com>
2023-10-13 23:53:14 +0900

    [#10-2]MinPerimeterRectangle

--- src/main/java/primeAndCompositeNumbers/Main.java
+++ src/main/java/primeAndCompositeNumbers/Main.java
@@ -7,5 +7,9 @@ public class Main {
         CountFactors countFactors = new CountFactors();
         int N1 = 24;
         System.out.println("=====> result: " + countFactors.solution(N1));
+
+        MinPerimeterRectangle minPerimeterRectangle = new MinPerimeterRectangle();
+        int N2 = 36;
+        System.out.println("=====> result: " + minPerimeterRectangle.solution(N2));
     }
 }
